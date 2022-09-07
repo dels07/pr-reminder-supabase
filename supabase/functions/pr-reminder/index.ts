e034c030e3314fa7a7c27150af4a3800a6305022
Kang Dels <crushfire2004@gmail.com>
2022-09-07 22:50:20 +0700

    feat: add pr-reminder edge function

--- supabase/functions/pr-reminder/index.ts
+++ supabase/functions/pr-reminder/index.ts
@@ -1,25 +1,30 @@
-// Follow this setup guide to integrate the Deno language server with your editor:
-// https://deno.land/manual/getting_started/setup_your_environment
-// This enables autocomplete, go to definition, etc.
+import { serve } from "https://deno.land/std@0.131.0/http/server.ts";
 
-import { serve } from "https://deno.land/std@0.131.0/http/server.ts"
-
-console.log("Hello from Functions!")
+import { execute } from "./service.ts";
+import { corsHeaders } from "../_shared/utils.ts";
 
 serve(async (req) => {
-  const { name } = await req.json()
-  const data = {
-    message: `Hello ${name}!`,
+  if (req.method === "OPTIONS") {
+    return new Response("ok", { headers: corsHeaders });
   }
 
-  return new Response(
-    JSON.stringify(data),
-    { headers: { "Content-Type": "application/json" } },
-  )
-})
+  try {
+    const results = await execute();
 
-// To invoke:
-// curl -i --location --request POST 'http://localhost:54321/functions/v1/' \
-//   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24ifQ.625_WdcF3KHqz5amU0x2X5WWHP-OEs_4qj0ssLNHzTs' \
-//   --header 'Content-Type: application/json' \
-//   --data '{"name":"Functions"}'
+    return new Response(
+      JSON.stringify(results),
+      {
+        headers: { ...corsHeaders, "Content-Type": "application/json" },
+        status: 200.,
+      },
+    );
+  } catch (error) {
+    return new Response(
+      error,
+      {
+        headers: { ...corsHeaders, "Content-Type": "application/json" },
+        status: 400,
+      },
+    );
+  }
+});
---
+++ supabase/functions/pr-reminder/service.ts
@@ -0,0 +1,83 @@
+import { getOpenPullRequests } from "../_shared/bitbucket.ts";
+import { sendMessage } from "../_shared/flock.ts";
+import { jakartaTime } from "../_shared/utils.ts";
+import {
+  getConfigs,
+  getGreeter,
+  getRandomGreeter,
+} from "../_shared/supabase.ts";
+
+type FnResult = {
+  type: string;
+  time: string;
+  repository: string;
+  open_pull_request: number;
+};
+
+export const execute = async (): Promise<FnResult[] | void> => {
+  const configs = await getConfigs();
+
+  if (!configs) {
+    return;
+  }
+
+  const results: FnResult[] = [];
+
+  for (const config of configs) {
+    const pullRequests = await getOpenPullRequests({
+      baseUrl: Deno.env.get("BITBUCKET_BASE_URL")!,
+      repository: config.bitbucket_repository,
+      username: config.bitbucket_username,
+      password: config.bitbucket_password,
+      authors: config.pr_authors?.split(",") ?? [],
+      interval: 1,
+    });
+
+    const result = {
+      type: "regular",
+      time: jakartaTime(),
+      repository: config.bitbucket_repository,
+      open_pull_request: pullRequests?.length ?? 0,
+    };
+
+    results.push(result);
+
+    console.info(
+      `[${result.time}][${result.repository}] Found ${result.open_pull_request} PR(s)`,
+    );
+
+    if (!pullRequests.length) {
+      continue;
+    }
+
+    await Promise.allSettled(
+      pullRequests.map(async ({ title, author, url, target }) => {
+        if (
+          target.search("master|release") !== -1 && config.flock_review_channel
+        ) {
+          const greeter = await getGreeter();
+          const message = `<flockml>${
+            greeter?.message ?? ""
+          }<br/><a href="${url}">${title}</a> by ${author}</flockml>`;
+
+          await sendMessage({
+            baseUrl: Deno.env.get("FLOCK_BASE_URL")!,
+            channel: config.flock_review_channel,
+          }, message);
+        }
+
+        const greeter = await getRandomGreeter();
+        const message = `<flockml>${
+          greeter?.message ?? ""
+        }<br/><a href="${url}">${title}</a> by ${author}</flockml>`;
+
+        await sendMessage({
+          baseUrl: Deno.env.get("FLOCK_BASE_URL")!,
+          channel: config.flock_channel,
+        }, message);
+      }),
+    );
+  }
+
+  return results;
+};
